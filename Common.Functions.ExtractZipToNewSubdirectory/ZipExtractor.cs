087c799440649c42139defa49ea1bb2f6742e99e
Christopher Dorst <chris@dorst.email>
2017-12-29 08:11:13 -0600

    Add ZipExtractor

---
+++ Common.Functions.ExtractZipToNewSubdirectory/ZipExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.IO.Path;
+using static System.IO.Compression.ZipFile;
+
+namespace Common.Functions.ExtractZipToNewSubdirectory
+{
+    public static class ZipExtractor
+    {
+        public static string Extract(string zipAbsolutePath)
+        {
+            var extractTo = GetNewSubdirectoryPath(zipAbsolutePath);
+            ExtractToDirectory(zipAbsolutePath, extractTo);
+            return extractTo;
+        }
+
+        private static string GetNewSubdirectoryPath(string filePath)
+            => Combine(
+                GetDirectoryName(filePath),
+                GetLast12CharsFromRandomGuid());
+
+        private static string GetLast12CharsFromRandomGuid()
+            => Guid.NewGuid().ToString().Substring(24);
+    }
+}
